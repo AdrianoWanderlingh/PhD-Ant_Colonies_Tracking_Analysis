6c6e038ee5443b0710dc4a2d6c57edc990743bcc
AdrianoWanderlingh <47888790+AdrianoWanderlingh@users.noreply.github.com>
2022-02-08 15:59:10 +0000

    Add files via upload

---
+++ Myrmidon_files_exp1/NURSE_ID_list/R7BS_09-04-21.0000_nurseid_folder/Define_Ant_Identifications.R
@@ -0,0 +1,19 @@
+#Open Experiment
+tracking_data <- fmExperimentOpen("R7BS_09-04-21.0000_nurseid.myrmidon") #if it says it's locked, click Session>Restart R and clear objects from workspace including hidden ones
+##GENERAL INFO
+tracking_data$getDataInformations()
+
+##user_defined_ant_pose.R
+ts <- fmQueryComputeTagStatistics(tracking_data)
+  
+for ( i in 1:nrow(ts)) {
+  # optionnaly maybe skip the tag with some criterion
+  if ( ts[i,"count"] >= 0.0001*max(ts[,"count"],na.rm=T) ) {
+    a <- tracking_data$createAnt();
+    tracking_data$addIdentification(tracking_data$cAnts()$summary[nrow(tracking_data$cAnts()$summary),"antID"],ts[i,"tagDecimalValue"],fmTimeInf(),fmTimeInf())
+  }
+}
+
+tracking_data$save("/media/adriano/DISK3/ADRIANO/NURSEID/R7BS_09-04-21.0000_nurseid_folder/R7BS_09-04-21.0000_nurseid.myrmidon")
+rm(list=c("tracking_data"))
+gc()
