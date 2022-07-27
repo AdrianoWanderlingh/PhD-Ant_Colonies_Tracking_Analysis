a09a0f4930b44023b4f20d65902a37c783c369c8
AdrianoWanderlingh <47888790+AdrianoWanderlingh@users.noreply.github.com>
2022-07-27 10:36:21 +0100

    Add files via upload

--- scriptsR/plot_outputs/Plot_Isolation_pre-post.R
+++ scriptsR/plot_outputs/Plot_Isolation_pre-post.R
@@ -17,7 +17,7 @@ library(dplyr)
 WORKDIR <- "/home/cf19810/Documents/scriptsR/EXP1_base_analysis"
 DATADIR <-  "/home/cf19810/Documents/scriptsR/EXP1_base_analysis/Data"
 
-SpaceUse <- read.table(paste(WORKDIR,"/Data/AntTasks_SpaceUse.txt",sep=""),header=T,stringsAsFactors = F, sep=",")
+SpaceUse <- read.table(paste(WORKDIR,"/Data/AntTasks_SpaceUse_july2022.txt",sep=""),header=T,stringsAsFactors = F, sep=",")
 
 ## Colony sizes info
 SpaceUse$treat <- str_sub(SpaceUse$colony,-2,-1)
@@ -33,11 +33,31 @@ SpaceUse <- SpaceUse[which(!is.na(SpaceUse$Exposed)),]
 
 N_exp <- as.data.frame(table(SpaceUse$Exposed))
 
+
+#mean by colony, AntTask, exposed, treatment
+
+
+Mean_SpaceUse <- aggregate(delta_time_inside ~ colony + AntTask + Exposed + treatment, FUN=mean, na.action=na.omit, SpaceUse)
+SE_SpaceUse <- aggregate(delta_time_inside ~ colony + AntTask + Exposed + treatment, FUN=std.error, na.action=na.omit, SpaceUse); colnames(SD_ants_exp) [match("colony_size",colnames(SD_ants_exp))] <- "SD_received"
+
+
+
+
 #### PRODUCE BOXPLOT OF USED SPACE FOR NURSES DELTA TIME INSIDE
-ggplot(SpaceUse, aes(x=treatment, y=delta_time_inside,color=AntTask))+
+ggplot(Mean_SpaceUse, aes(x=treatment, y=delta_time_inside,color=AntTask))+
  # geom_jitter(aes(fill = AntTask))+
-  #geom_point(position=position_jitterdodge())+
   geom_boxplot()+
+  geom_point(position=position_jitterdodge(), size=0.8)+
+  facet_wrap(~Exposed) +
+  labs(title= "SpaceUse",
+       subtitle=paste("N",N_exp[1,1],":",N_exp[1,2],";","N",N_exp[2,1],N_exp[1,1],":",N_exp[2,2]))
+
+
+#### SAME PLOT WITHOUT TASK GROUPING
+ggplot(Mean_SpaceUse, aes(x=treatment, y=delta_time_inside))+
+  # geom_jitter(aes(fill = AntTask))+
+  geom_boxplot()+
+  geom_point(size=0.8)+
   facet_wrap(~Exposed) +
   labs(title= "SpaceUse",
        subtitle=paste("N",N_exp[1,1],":",N_exp[1,2],";","N",N_exp[2,1],N_exp[1,1],":",N_exp[2,2]))
